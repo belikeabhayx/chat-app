17139cd0a2e38ddde21dd6538e37d45347f7e824
abhay chauhan <chauhanabhay.chauhan2@gmail.com>
2025-12-26 18:56:24 +0530

    feat: add redis realtime

--- src/app/api/[[...slugs]]/route.ts
+++ src/app/api/[[...slugs]]/route.ts
@@ -1,21 +1,65 @@
 import { redis } from "@/src/lib/redis";
 import { Elysia, t } from "elysia";
 import { nanoid } from "nanoid";
+import { authMiddleware } from "./auth";
+import z from "zod";
+import { Message, realtime } from "@/src/lib/realtime";
 
-const ROOM_TTL_SECONDS = 60 * 10
+const ROOM_TTL_SECONDS = 60 * 10;
 
-const rooms = new Elysia({ prefix: "/room" }).post("/create", async() => {
-  const roomId = nanoid()
+const rooms = new Elysia({ prefix: "/room" }).post("/create", async () => {
+  const roomId = nanoid();
 
   await redis.hset(`meta:${roomId}`, {
     connected: [],
     createdAt: Date.now(),
-  })
+  });
 
-  await redis.expire(`meta:${roomId}`, ROOM_TTL_SECONDS)
+  await redis.expire(`meta:${roomId}`, ROOM_TTL_SECONDS);
 
-  return {roomId}
+  return { roomId };
 });
+
+const messages = new Elysia({ prefix: "/messages" }).use(authMiddleware).post(
+  "/",
+  async ({ body, auth }) => {
+    const { sender, text } = body;
+    const { roomId } = auth;
+    const roomExists = await redis.exists(`meta:${roomId}`);
+
+    if (!roomExists) {
+      throw new Error("Room does not exist");
+    }
+
+    const message: Message = {
+      id: nanoid(),
+      sender,
+      text,
+      timestamp: Date.now(),
+      roomId,
+      token: auth.token,
+    };
+
+    // add message to history
+    await redis.rpush(`messages:${roomId}`, { ...message, token: auth.token });
+    await realtime.channel(roomId).emit("chat.message", message);
+
+    //self destruct
+    const remaining = await redis.ttl(`meta:${roomId}`);
+    await redis.expire(`messages:${roomId}`, remaining);
+    await redis.expire(`history:${roomId}`, remaining);
+    await redis.expire(`meta:${roomId}`, remaining);
+  },
+  {
+    query: z.object({
+      roomId: z.string(),
+    }),
+    body: z.object({
+      sender: z.string().max(100),
+      text: z.string().max(1000),
+    }),
+  }
+);
 
 export const app = new Elysia({ prefix: "/api" }).use(rooms);
 
